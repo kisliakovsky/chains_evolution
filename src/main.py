5956490a220025ef063b961d558834144ebfdf95
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-01 10:51:12 +0300

    Favorite pathway.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from src.clusters_info import calc_cluster_distribution
 from src.pathways import collect_pathways
 
 NUMBER_OF_CLUSTERS = 10
 POPULATION_SIZE = 100
 
-CACHE = True
+CACHE = False
 CACHED = {
     "distribution": [1, 34, 14, 4, 19, 8, 2, 6, 6, 6],
     "pathways": ['XAFIFEDY', 'XANFEY', 'XANFEY', 'XANFEY', 'XAFNIFEY', 'XAFNFEY', 'XAFNFEY', 'XAFNFEY', 'XAFNFEY',
@@ -18,17 +20,26 @@ CACHED = {
                  'XAFNIFEY', 'XANIFEDY', 'XAFIFEDY', 'XAFIFEDY', 'XAFIFEDY', 'XAFIFEDY', 'XAFIFDY', 'XADY', 'XADY',
                  'XAFNIFEDY', 'XAFNIFEDY', 'XAIFEY', 'XAIFEY', 'XAIFEY', 'XAIFY', 'XAIEY', 'XAFIEY', 'XAFNIFY',
                  'XAFNFDY', 'XAFNFDY', 'XAFEIEDY', 'XAFEFDY', 'XAFEDY', 'XAFNIFEDEY', 'XAFNIFEDEY', 'XAFNIFEDEY',
-                 'XAFNIFEDEY', 'XAFNIFEDEY', 'XAFNFEY']
+                 'XAFNIFEDEY', 'XAFNIFEDEY', 'XAFNFEY'],
+    "favorite_pathway": 'XAFNIFEIFEY'
 }
 
 
 def main():
     if CACHE:
         pathways = CACHED["pathways"]
+        favorite_pathway = CACHED["favorite_pathway"]
     else:
         cluster_distribution = calc_cluster_distribution(POPULATION_SIZE, random_seed=47)
         pathways = collect_pathways(cluster_distribution)
+        favorite_pathway = select_favorite_pathway(pathways)
     print(pathways)
+    print(favorite_pathway)
+
+
+def select_favorite_pathway(pathways: List[str]):
+    index = len(pathways) // 2
+    return pathways[index]
 
 
 if __name__ == '__main__':
