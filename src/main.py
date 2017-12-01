92bf9d594fd8e20079301efaed8da76c457ea505
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-01 10:41:01 +0300

    Pathway generation done.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-from numpy.random import RandomState
-
 from src.clusters_info import calc_cluster_distribution
-from src.transition_info import obtain_sources_and_transition_probabilities
+from src.pathways import collect_pathways
 
 NUMBER_OF_CLUSTERS = 10
 POPULATION_SIZE = 100
@@ -15,47 +13,8 @@ def main():
         cluster_distribution = CACHED_CLUSTER_DISTRIBUTION
     else:
         cluster_distribution = calc_cluster_distribution(POPULATION_SIZE, random_seed=47)
-    for i in range(NUMBER_OF_CLUSTERS):
-        cluster_size = cluster_distribution[i]
-        sources, transition_probabilities_by_sources = obtain_sources_and_transition_probabilities(i)
-        pathway_counter = 0
-        pathways = []
-        for _ in range(cluster_size):
-            pathways.append(["X"])
-        while pathway_counter < cluster_size:
-            pathway = pathways[pathway_counter]
-            next_source = pathway[-1]
-            if next_source == "Y":
-                pathway_counter += 1
-                continue
-            else:
-                transition_probabilities = transition_probabilities_by_sources[next_source]
-                random = RandomState()
-                target_distribution = random.multinomial(cluster_size, transition_probabilities, size=1)[0]
-
-
-
-
-
-
-
-
-
-
-        # pathways = []
-        # for each in range(1):  # TODO: There should be while loop. To execute while last item of pathway does not equal 'Y'".
-        #     pathway = ["X"]
-        #     key = pathway[-1]
-        #     transition_probabilities = transition_probabilities_by_sources[key]
-        #     random = RandomState()
-        #     next_location_sizes = random.multinomial(cluster_size, transition_probabilities, size=1)[0]
-        #     for source_index, next_location_size in enumerate(next_location_sizes):
-        #         for _ in range(next_location_size):
-        #             new_pathway = pathway[:]
-        #             new_pathway.append(sources[source_index][0])
-        #             pathways.append(new_pathway)
-        #     print(pathways)
-        #     pass  # TODO: Implement a pathway generation.
+    pathways = collect_pathways(cluster_distribution)
+    print(pathways)
 
 
 if __name__ == '__main__':
--- src/transition_info.py
+++ src/transition_info.py
@@ -6,20 +6,19 @@ from pandas import DataFrame
 from numpy import ndarray as NDArray
 
 from src import paths
-from src.pathways import FINISH_LOCATION, START_LOCATION
 
 TRANSITION_MATRIX_FILE_NAME = "Transition_matrix"
 
 
-def obtain_sources_and_transition_probabilities(index: int) -> Tuple[List[str], Dict[str, NDArray]]:
+def obtain_sources_and_transition_probabilities(index: int, start: str, finish: str) -> Tuple[List[str], Dict[str, NDArray]]:
     file_name = _build_file_name(index)
     dataframe = _load_transition_matrix_file(file_name)
     sources = []
     transition_probabilities_by_sources = {}
     for source, transition_probabilities in dataframe.iterrows():
         source = {
-            "_01": START_LOCATION,
-            "*01": FINISH_LOCATION
+            "_01": start,
+            "*01": finish
         }.get(source, source)
         sources.append(source)
         transition_probabilities = transition_probabilities.as_matrix()
