d74f264c383d021a66cbfbdf5888f6f34850abc3
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-02 17:22:57 +0300

    Graph exporting.

--- src/graph_building.py
+++ src/graph_building.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, List, Union, Tuple
+from typing import Dict, List, Union, Tuple, Callable
 
 from pandas import DataFrame
 
@@ -14,12 +14,85 @@ def build_for_cytoscape(sequences: List[str]) -> Union[str, None]:
 
 def _build_json(sequences: List[str]) -> str:
     dictionary = build_as_dict(sequences)
-    return json.dumps(dictionary, indent=4)
+    return json.dumps(dictionary, indent=4, separators=(',', ': '))
 
 
-def build_as_dict(sequences: List[str]) -> List[Element]:
+def build_as_dict(sequences: List[str], step_name: str) -> List[Element]:
     elements = []
-    _add_nodes(elements, sequences)
+    _add_nodes(elements, sequences, step_name)
+
+    def handler(source: str, target: str, weight: int):
+        elements.append(_create_edge(source, target, weight))
+    _map_edge_components(sequences, handler)
+    return elements
+
+
+def _add_nodes(elements: List[Element], sequences: List[str], step_name: str):
+    for sequence in sequences:
+        elements.append(_create_node(sequence, step_name))
+
+
+def _create_node(identifier: str, step_name: str) -> Element:
+    return {
+        "data": {"id": identifier}
+    }
+
+
+def _create_edge(source: str, target: str, weight: int) -> Element:
+    return {
+        "data": {
+            "id": "{}_{}".format(source,target),
+            "source": source,
+            "target": target,
+            "weight": weight
+        }
+    }
+
+
+def build_as_dataframes(sequences: List[str], step_name: str) -> Tuple[DataFrame, DataFrame]:
+    nodes = _create_node_dataframe(sequences, step_name)
+    edges = _create_edge_dataframe(sequences)
+    return nodes, edges
+
+
+def _create_node_dataframe(sequences: List[str], step_name: str):
+    nodes = {
+        "Id": sequences,
+        "Label": sequences,
+        "Class": [step_name for _ in sequences]
+    }
+    return DataFrame(data=nodes)
+
+
+def _create_edge_dataframe(sequences: List[str]):
+    sources = []
+    targets = []
+    types = []
+    ids = []
+    weights = []
+
+    def handler(source: str, target: str, weight: str):
+        sources.append(source)
+        targets.append(target)
+        types.append("Undirected")
+        ids.append("{}_{}".format(source, target))
+        weights.append(weight)
+
+    # PyCharm bug
+    # noinspection PyTypeChecker
+    _map_edge_components(sequences, handler)
+    edges = {
+        "Source": sources,
+        "Target": targets,
+        "Type": types,
+        "Id": ids,
+        "Label": weights,
+        "Weight": weights
+    }
+    return DataFrame(data=edges)
+
+
+def _map_edge_components(sequences: List[str], handler: Callable[[str, str, int], None]):
     distances = calculate_distances(sequences)
     number_of_rows = len(distances)
     number_of_columns = len(distances[0])
@@ -29,31 +102,4 @@ def build_as_dict(sequences: List[str]) -> List[Element]:
             if weight > 0:
                 source = sequences[i]
                 target = sequences[j]
-                elements.append(_create_edge(source, target, weight))
-    return elements
-
-
-def _add_nodes(elements: List[Element], sequences: List[str]):
-    for sequence in sequences:
-        elements.append(_create_node(sequence))
-
-
-def _create_node(identifier: str) -> Element:
-    return {
-        "data": {"id": identifier}
-    }
-
-
-def _create_edge(source: str, target: str, weight: int) -> Element:
-    return {
-        "data": {
-            "id": source + target,
-            "source": source,
-            "target": target,
-            "weight": weight
-        }
-    }
-
-
-def build_as_dataframes(sequences: List[str]) -> Tuple[DataFrame, DataFrame]:
-    return DataFrame({'A': []}), DataFrame({'A': []})
+                handler(source, target, weight)
--- src/graph_exporting.py
+++ src/graph_exporting.py
@@ -9,29 +9,29 @@ EXPORT_DIR = Path("../export")
 
 
 def save_for_cytoscape(sequences: List[str], step_name: str):
-    section_path = paths.build_section_dir_path(step_name)
-    section_path.mkdir(exist_ok=True)
-    _save_json(sequences, "forCytoscape", section_path)
+    _save_json(sequences, "forCytoscape", step_name)
 
 
 def save_for_gephi(sequences: List[str], step_name: str):
+    _save_csvs(sequences, "forGephi", step_name)
+
+
+def _save_json(sequences: List[str], file_name: str, step_name: str):
     section_path = paths.build_section_dir_path(step_name)
     section_path.mkdir(exist_ok=True)
-    _save_csvs(sequences, "forGephi", section_path)
-
-
-def _save_json(sequences: List[str], file_name: str, section_path: Path):
-    dictionary = graph_building.build_as_dict(sequences)
+    dictionary = graph_building.build_as_dict(sequences, step_name)
     file_path = paths.build_output_graph_path(section_path, file_name)
     with open(str(file_path), "w") as file:
-        json.dump(dictionary, file)
+        json.dump(dictionary, file,  indent=4, separators=(',', ': '))
 
 
-def _save_csvs(sequences: List[str], file_name: str, section_path: Path):
-    node_dataframe, edge_dataframe = graph_building.build_as_dataframes(sequences)
+def _save_csvs(sequences: List[str], file_name: str, step_name: str):
+    section_path = paths.build_section_dir_path(step_name)
+    section_path.mkdir(exist_ok=True)
+    node_dataframe, edge_dataframe = graph_building.build_as_dataframes(sequences, step_name)
     subsection_path = paths.build_subsection_dir_path(section_path, file_name)
     subsection_path.mkdir(exist_ok=True)
     node_file_path = paths.build_output_table_path(subsection_path, "nodes".format(file_name))
     edge_file_path = paths.build_output_table_path(subsection_path, "edges".format(file_name))
-    node_dataframe.to_csv(str(node_file_path))
-    edge_dataframe.to_csv(str(edge_file_path))
+    node_dataframe.to_csv(str(node_file_path), index=False)
+    edge_dataframe.to_csv(str(edge_file_path), index=False)
