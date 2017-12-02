e09d3a5b923c1ea51a0c6689fa3b24df8c84f4f2
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-02 22:22:54 +0300

    Base for gene probabilities determination.

--- src/evolution.py
+++ src/evolution.py
@@ -4,6 +4,7 @@ from numpy.random import RandomState
 
 from src import strings, maths
 
+Letter = str
 
 def obtain_evolution_subsequences(sequence: str):
     subsequences = strings.obtain_forward_substrings(sequence)
@@ -15,21 +16,34 @@ class ChildGenerator(object):
     def __init__(self, parents: List[str]):
         parents = ["XABCY", "XDEFGY", "XHIJKLMY", "XHIFGY", "XABJKLMY"]
         self.__unique_genes = [parent[1:-1] for parent in parents]
+        self.__gene_lengths = self.__get_gene_lengths()
+        self.__unique_lengths, self.__lengths_repetitions = self.__get_length_repetitions()
         self.__random = RandomState()
+        self.__length_probabilities = self.__determine_length_probabilities()
+        self.__gene_probabilities = self.__determine_gene_probabilities()
 
     def generate(self) -> str:
         length = self.__determine_length()
         child = ""
         for i in range(length):
-            child += "A"
+            child += self.__produce_gene()
         return "X" + child + "Y"
 
+    def __produce_gene(self) -> Letter:
+        return "A"
+
+    def __get_gene_lengths(self):
+        return [len(gene) for gene in self.__unique_genes]
+
+    def __get_length_repetitions(self):
+        return maths.count_repetitions(sorted(self.__gene_lengths))
+
+    def __determine_length_probabilities(self):
+        return [repetition / len(self.__gene_lengths) for repetition in self.__lengths_repetitions]
+
     def __determine_length(self):
-        lengths = [len(gene) for gene in self.__unique_genes]
-        unique_lengths, lengths_repetitions = maths.count_repetitions(sorted(lengths))
-        lengths_probabilities = [repetition / len(lengths) for repetition in lengths_repetitions]
-        length_index = list(self.__random.multinomial(1, lengths_probabilities, size=1)[0]).index(1)
-        return unique_lengths[length_index]
+        index = list(self.__random.multinomial(1, self.__length_probabilities, size=1)[0]).index(1)
+        return self.__unique_lengths[index]
 
-
-print(ChildGenerator([]).generate())
+    def __determine_gene_probabilities(self):
+        pass
