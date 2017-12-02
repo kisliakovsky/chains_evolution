ce1bb23d67c9d551f32ebfa550b7142e3d9dbd4d
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-02 17:25:05 +0300

    Evolution base.

--- src/evolution.py
+++ src/evolution.py
@@ -1,6 +1,17 @@
+from typing import List
+
 from src import strings
 
 
 def obtain_evolution_subsequences(sequence: str):
     subsequences = strings.obtain_forward_substrings(sequence)
     return subsequences[2:]
+
+
+class ChildGenerator(object):
+
+    def __init__(self, parents: List[str]):
+        self.__parents = parents
+
+    def generate(self) -> str:
+        pass
