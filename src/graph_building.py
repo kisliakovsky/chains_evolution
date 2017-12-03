0a2829264c7d0f3a25da5d36ffdaddd9e69af2f9
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-03 16:48:23 +0300

    Reduced max edge length (weight)

--- src/graph_building.py
+++ src/graph_building.py
@@ -6,15 +6,15 @@ from pandas import DataFrame
 from src.distances import calculate_distances
 
 Element = Dict[str, Dict[str, str]]
-MAX_EDGE_WEIGHT = 6
+MAX_EDGE_WEIGHT = 4
 
 
-def build_for_cytoscape(sequences: List[str]) -> Union[str, None]:
-    return _build_json(sequences)
+def build_for_cytoscape(sequences: List[str], step_name: str) -> Union[str, None]:
+    return _build_json(sequences, step_name)
 
 
-def _build_json(sequences: List[str]) -> str:
-    dictionary = build_as_dict(sequences)
+def _build_json(sequences: List[str], step_name: str) -> str:
+    dictionary = build_as_dict(sequences, step_name)
     return json.dumps(dictionary, indent=4, separators=(',', ': '))
 
 
@@ -52,7 +52,7 @@ def _create_edge(source: str, target: str, weight: int) -> Element:
 
 def build_as_dataframes(sequences: List[str], step_name: str) -> Tuple[DataFrame, DataFrame]:
     nodes = _create_node_dataframe(sequences, step_name)
-    edges = _create_edge_dataframe(sequences)
+    edges = _create_edge_dataframe(sequences, step_name)
     return nodes, edges
 
 
@@ -65,7 +65,7 @@ def _create_node_dataframe(sequences: List[str], step_name: str):
     return DataFrame(data=nodes)
 
 
-def _create_edge_dataframe(sequences: List[str]):
+def _create_edge_dataframe(sequences: List[str], step_name: str):
     sources = []
     targets = []
     types = []
@@ -88,7 +88,8 @@ def _create_edge_dataframe(sequences: List[str]):
         "Type": types,
         "Id": ids,
         "Label": weights,
-        "Weight": weights
+        "Weight": weights,
+        "Class": step_name
     }
     return DataFrame(data=edges)
 
