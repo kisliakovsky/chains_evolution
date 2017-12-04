64c13e15c9ca1859f329be841e679fd0522d91d6
kisliakovsky <kisliakovskiii@yandex.ru>
2017-12-04 08:02:38 +0300

    Elimination of Single choice repetition.

--- src/evolution.py
+++ src/evolution.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from numpy.random import RandomState
 
@@ -26,13 +26,26 @@ class ChildGenerator(object):
         length = self.__determine_length()
         child = ""
         for i in range(length):
-            child += self.__produce_gene(i)
+            child += self.__produce_letter(i, i == (length - 1))
         return "X" + child + "Y"
 
-    def __produce_gene(self, position: int) -> Letter:
-        codes, probabilities, random = self.__code_probabilities_by_position[position]
-        index = list(random.multinomial(1, probabilities, size=1)[0]).index(1)
-        return codes[index]
+    def __produce_letter(self, position: int, is_last: bool) -> Letter:
+        curr_codes, curr_probabilities, curr_random = self.__code_probabilities_by_position[position]
+        if not is_last:
+            next_codes, next_probabilities, next_random = self.__code_probabilities_by_position[position + 1]
+            if len(next_probabilities) == 1:
+                next_code = next_codes[0]
+                i = curr_codes.index(next_code) if next_code in curr_codes else None
+                if i is not None:
+                    p = curr_probabilities[i] / (len(curr_probabilities) - 1)
+                    new_curr_codes = curr_codes[:]
+                    new_curr_probabilities = [curr_probability + p for curr_probability in curr_probabilities]
+                    del new_curr_probabilities[i]
+                    del new_curr_codes[i]
+                    index = list(curr_random.multinomial(1, new_curr_probabilities, size=1)[0]).index(1)
+                    return new_curr_codes[index]
+        index = list(curr_random.multinomial(1, curr_probabilities, size=1)[0]).index(1)
+        return curr_codes[index]
 
     def __get_gene_lengths(self):
         return [len(gene) for gene in self.__unique_genes]
@@ -47,7 +60,7 @@ class ChildGenerator(object):
         index = list(self.__random.multinomial(1, self.__length_probabilities, size=1)[0]).index(1)
         return self.__unique_lengths[index]
 
-    def __determine_code_probabilities_by_position(self):
+    def __determine_code_probabilities_by_position(self) -> List[Tuple[List[Letter], List[float], RandomState]]:
         max_length = max(self.__unique_lengths)
         all_code_probabilities = []
         for position in range(max_length):
